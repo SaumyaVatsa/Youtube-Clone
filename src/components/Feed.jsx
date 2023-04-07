b02f418e977222d2fc476b0b643638487669d0a6
Saumya Vatsa <saumyavatsa2108@gmail.com>
2023-04-08 00:05:28 +0530

    Feeds Completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -22,7 +22,7 @@ const Feed = () => {
         </Typography>
       </Box>
 
-      <Box p={2} sx={{OverflowY: 'auto', height: '90vh', flex: 2}}>
+      <Box p={2} sx={{overflowY: 'auto',height:'90vh', flex: 2}}>
         <Typography variant="h4" fontWeight='bold' mb={2} sx={{color: 'white'}}>
           {selectedButton} <span style={{color: '#fc1503'}}>Videos</span>
         </Typography>
